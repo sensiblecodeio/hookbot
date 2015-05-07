d00c218bdc21e202cf10ea4034d60a8227fed821
Peter Waller <p@pwaller.net>
2015-05-07 18:42:27 +0100

    Add test auth: missing, invalid, valid

--- auth_test.go
+++ auth_test.go
@@ -7,37 +7,107 @@ import (
 	"testing"
 )
 
+const (
+	TEST_GITHUB_SECRET = "github_secret"
+	TEST_KEY           = "key"
+)
+
+func MakePubRequest(url, body string) (w *httptest.ResponseRecorder, r *http.Request) {
+	w = httptest.NewRecorder()
+	bodyReader := bytes.NewReader([]byte(body))
+	r, _ = http.NewRequest("POST", "http://localhost"+url, bodyReader)
+	return w, r
+}
+
 // Unsafe pub should always succeed, without credentials
 func TestUnsafePub(t *testing.T) {
 
 	run := func(iteration int) {
-		hookbot := NewHookbot("key", "github_secret")
 
-		msgs := hookbot.Add("/unsafe/")
-		defer hookbot.Del(msgs)
+		w, r := MakePubRequest("/unsafe/pub/", "MESSAGE")
 
-		w := httptest.NewRecorder()
+		var c chan []byte
 
-		body := bytes.NewReader([]byte("MESSAGE"))
-		r, _ := http.NewRequest("POST", "http://localhost/unsafe/pub/", body)
+		func() {
+			hookbot := NewHookbot(TEST_KEY, TEST_GITHUB_SECRET)
+			defer hookbot.Shutdown()
 
-		hookbot.ServeHTTP(w, r)
+			msgs := hookbot.Add("/unsafe/")
+			defer hookbot.Del(msgs)
+			c = msgs.c
 
-		hookbot.Shutdown()
+			hookbot.ServeHTTP(w, r)
+		}()
 
 		if w.Code != http.StatusOK {
 			t.Errorf("Status code != 200 (= %v)", w.Code)
 		}
 
+		// Message should have been delivered by the time we see
+		// hookbot.Shutdown().
 		select {
-		case <-msgs.c:
+		case <-c:
 		default:
 			t.Fatalf("Message not delivered (iteration %v)", iteration)
 		}
 	}
 
-	// Run the test repeatedly to search for races
+	// Run the test repeatedly to search for races.
 	for i := 0; i < 10; i++ {
 		run(i)
+	}
+}
+
+// Bad authentication should cause a 404 not found.
+func TestAuthMissingFail(t *testing.T) {
+	w, r := MakePubRequest("/pub/", "MESSAGE")
+
+	func() {
+		hookbot := NewHookbot(TEST_KEY, TEST_GITHUB_SECRET)
+		defer hookbot.Shutdown()
+
+		hookbot.ServeHTTP(w, r)
+	}()
+
+	if w.Code != http.StatusNotFound {
+		t.Errorf("Status code != 404 (= %v)", w.Code)
+	}
+}
+
+// Invalid secret authentication should return 404 Not Found.
+func TestAuthInvalidSecret(t *testing.T) {
+	w, r := MakePubRequest("/pub/", "MESSAGE")
+
+	token := Sha1HMAC(TEST_KEY, "/pub/not/the/same/as/above") // bad secret
+	r.SetBasicAuth(token, "")
+
+	func() {
+		hookbot := NewHookbot(TEST_KEY, TEST_GITHUB_SECRET)
+		defer hookbot.Shutdown()
+
+		hookbot.ServeHTTP(w, r)
+	}()
+
+	if w.Code != http.StatusNotFound {
+		t.Errorf("Status code != 404 (= %v)", w.Code)
+	}
+}
+
+// Valid secret authentication should return 200 OK
+func TestAuthSuccess(t *testing.T) {
+	w, r := MakePubRequest("/pub/place", "MESSAGE")
+
+	token := Sha1HMAC(TEST_KEY, "/pub/place")
+	r.SetBasicAuth(token, "")
+
+	func() {
+		hookbot := NewHookbot(TEST_KEY, TEST_GITHUB_SECRET)
+		defer hookbot.Shutdown()
+
+		hookbot.ServeHTTP(w, r)
+	}()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Status code != 200 (= %v)", w.Code)
 	}
 }
